7d580f896b2f38c597a354eef775dcc19747a750
Jesús Antonio <jesusgtz1@hotmail.com>
2025-11-12 16:39:53 -0600

    se actualiza unidad uno y se coloca codigo para dejar ser embebida esta aplicación

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.ts
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // o "/quizzes/:slug"
+        headers: [
+          // X-Frame-Options está deprecado, pero algunos navegadores lo respetan:
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          // Autoriza a Pixkay a embeber
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://pixkay.com https://*.pixkay.com https://app.pixkay.com;"
+          },
+        ],
+      },
+    ];
+  },
 };
-
 export default nextConfig;
